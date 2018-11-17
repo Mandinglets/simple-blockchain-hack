5e4493eebae95c4586042bcc2979258e2437af84
Phu Sakulwongtana <phusakulwongtana@gmail.com>
2018-11-17 11:49:14 +0000

    Adding python bug not fixed

--- client.py
+++ client.py
@@ -1,1 +1,45 @@
-# For Client Class
+# For Client Class -- Code from DAPs only
+import sys, asyncio
+
+class NoneException(Exception):
+    pass
+
+class ClosingException(Exception):
+    pass
+
+async def open_connection(loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1',
+                                                     8888,loop=loop)
+    return reader, writer
+
+async def use_connection(reader, writer):
+    try:
+        while True:
+            console_message = input('>> ')
+            if console_message == None or console_message == '':
+                continue
+            elif console_message == 'close()':
+                raise ClosingException()
+                
+            writer.write(console_message.encode())
+            data = await reader.read(100)
+            print('Received: %r' % data.decode())
+    except ClosingException:
+        print('Got close() from user.')
+    except KeyboardInterrupt:
+        print('Got Ctrl-C from user.')
+    finally:
+        writer.close()
+
+def run():
+    try:
+        loop = asyncio.get_event_loop()
+        reader,writer=loop.run_until_complete(open_connection(loop))
+        loop.run_until_complete(use_connection(reader, writer))
+    except Exception as e:
+        print(e,file=sys.stderr)
+    finally:
+        loop.close()
+
+if __name__ == '__main__':
+    run()
--- server.py
+++ server.py
@@ -1,3 +1,36 @@
-# For Server Class
-import asyncio
-import 
+# For Server Class -- Using the code from DAPs Class.
+import sys, asyncio
+
+all_clients = set([])
+
+async def handle_connection(reader, writer):
+    all_clients.add(writer)
+    client_addr = writer.get_extra_info('peername')
+    print(f"New client {client_addr}")
+
+    while True:
+        data = await reader.read(100)
+        for other_writer in all_clients:
+            other_writer.write(data)
+            await other_writer.drain()
+
+    print("Close the client socket")
+    writer.close()
+    all_clients.remove(writer)
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_connection,'127.0.0.1',
+                                            8888,loop=loop)
+server = loop.run_until_complete(coro)
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    print('\nGot keyboard interrupt, shutting down',file=sys.stderr)
+
+for task in asyncio.Task.all_tasks():
+    task.cancel()
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
